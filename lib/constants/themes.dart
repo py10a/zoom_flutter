615ee318b5ef4cc63d3053f0cf6c53e89706f563
Andriy Pylo <py10.dev@gmail.com>
2025-01-22 23:09:11 +0100

    Refine color scheme for light and dark themes
    This commit refines the color scheme for both light and dark themes, adjusting various color values to improve visual appeal and contrast.
    
    Specifically, the following changes were made:
    
    - In the light theme, the secondary color was changed to a lighter gray, the onSecondary color to dark gray, and the onSurface color to a darker gray.
    - In the dark theme, the secondary color was changed to a dark gray, the surface color to a darker gray, the onSecondary color to a lighter gray, and the onSurface color to a light gray.
    
    Signed-off-by: Andriy Pylo <py10.dev@gmail.com>

--- lib/constants/themes.dart
+++ lib/constants/themes.dart
@@ -12,12 +12,12 @@ final darkTheme = ThemeData(
 
 final lightColorScheme = ColorScheme(
   primary: Color(0xFF007AFF), // Darker Zoom blue
-  secondary: Color(0xFF201E20), // Darker Zoom green
+  secondary: Color(0xFFE6E5E5), // Darker Zoom green
   surface: Color(0xFFFFFFFF), // Light background
   error: Color(0xFFFF3B30), // Error red
   onPrimary: Color(0xFFFFFFFF), // White on primary
-  onSecondary: Color(0xFFFFFFFF), // White on secondary
-  onSurface: Color(0xFF000000), // Black on background
+  onSecondary: Color(0xFF2C2C2C), // White on secondary
+  onSurface: Color(0xFF161919), // Black on background
   onError: Color(0xFFFFFFFF), // White on error
   brightness: Brightness.light, // Light theme
   outline: Color(0xFF545458), // Dark gray
@@ -26,12 +26,12 @@ final lightColorScheme = ColorScheme(
 
 final darkColorScheme = ColorScheme(
   primary: Color(0xFF0A84FF), // Darker Zoom blue
-  secondary: Color(0xFF201E20), // Darker Zoom green
-  surface: Color(0xFF1C1C1E), // Dark background
+  secondary: Color(0xFF32393B), // Darker Zoom green
+  surface: Color(0xFF1D2121), // Dark background
   error: Color(0xFFFF453A), // Error red
   onPrimary: Color(0xFFFFFFFF), // White on primary
-  onSecondary: Color(0xFFFFFFFF), // White on secondary
-  onSurface: Color(0xFFFFFFFF), // White on background
+  onSecondary: Color(0xFFD5D5D5), // White on secondary
+  onSurface: Color(0xFFDFDFDF), // White on background
   onError: Color(0xFF000000), // Black on error
   brightness: Brightness.dark, // Dark theme
   outline: Color(0xFF545458), // Dark gray

1c865c99320867266f1a230fd71eb50ae20bf946
Andriy Pylo <py10.dev@gmail.com>
2025-01-29 00:21:52 +0100

    Refactor CommonButton widget to use ButtonStyle.
    Signed-off-by: Andriy Pylo <py10.dev@gmail.com>

--- lib/common/widgets/common_button.dart
+++ lib/common/widgets/common_button.dart
@@ -28,15 +28,54 @@ class CommonButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
-
     return FilledButton.icon(
       icon: icon,
-      style: FilledButton.styleFrom(
-        minimumSize: Size(double.infinity, 48),
-        backgroundColor: backgroundColor ?? theme.primary,
-        foregroundColor: foregroundColor ?? theme.onPrimary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+      style: ButtonStyle(
+        elevation: WidgetStateProperty<double?>.fromMap(
+          <WidgetStatesConstraint, double>{
+            WidgetState.disabled: 0,
+            WidgetState.hovered: 2,
+            WidgetState.pressed: 6,
+            WidgetState.any: 1,
+          },
+        ),
+        minimumSize: WidgetStateProperty.all(
+          Size(double.infinity, 48),
+        ),
+        backgroundColor: WidgetStateProperty<Color>.fromMap(
+          backgroundColor != null
+              ? <WidgetStatesConstraint, Color>{
+                  WidgetState.any: backgroundColor!,
+                  WidgetState.disabled: backgroundColor!.withAlpha(122),
+                  WidgetState.hovered: backgroundColor!.withAlpha(200),
+                  WidgetState.pressed: backgroundColor!.withAlpha(220),
+                }
+              : <WidgetStatesConstraint, Color>{
+                  WidgetState.any: theme.primary,
+                  WidgetState.disabled: theme.primary.withAlpha(122),
+                  WidgetState.hovered: theme.primary.withAlpha(200),
+                  WidgetState.pressed: theme.primary.withAlpha(220),
+                },
+        ),
+        foregroundColor: WidgetStateProperty<Color>.fromMap(
+          foregroundColor != null
+              ? <WidgetStatesConstraint, Color>{
+                  WidgetState.any: foregroundColor!,
+                  WidgetState.disabled: foregroundColor!.withAlpha(122),
+                  WidgetState.hovered: foregroundColor!.withAlpha(200),
+                  WidgetState.pressed: foregroundColor!.withAlpha(220),
+                }
+              : <WidgetStatesConstraint, Color>{
+                  WidgetState.any: theme.onPrimary,
+                  WidgetState.disabled: theme.onPrimary.withAlpha(122),
+                  WidgetState.hovered: theme.onPrimary.withAlpha(200),
+                  WidgetState.pressed: theme.onPrimary.withAlpha(220),
+                },
+        ),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
         ),
       ),
       onPressed: onPressed,
